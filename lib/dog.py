d76a791c4fca7d1ad5b2d9692f2645d9a23e7b3e
Eric Nelson <ernelson12o0@gmail.com>
2023-08-27 16:40:49 -0400

    Lab Complete

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,32 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name='name', breed='Pug'):
+        self._name = None
+        self._breed = None
+        self.name = name
+        self.breed = breed
+
+    def get_name(self):
+        return self._name
+            
+    def set_name(self, name):
+        if not isinstance(name, str) or not 1 <= len(name) <= 25:
+            print("Name must be string between 1 and 25 characters.") 
+        else:
+            self._name = name
+
+    name = property(get_name, set_name)
+
+    def get_breed(self):
+        return self._breed 
+
+    def set_breed(self, breed):
+        if breed not in APPROVED_BREEDS:
+            print('Breed must be in list of approved breeds.')
+        else:
+            self._breed = breed
+
+    breed = property(get_breed, set_breed)      
+
+fido = Dog("Fido")
